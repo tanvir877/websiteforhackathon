5d31d3ad14a6155dc8e61f2e493481d21e58eb40
tanvirhasan <tanvirhasan10902@gmail.com>
2025-02-25 09:51:44 +0600

    changed many thing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var video1 = document.querySelector("#video1")
+function video1animation(){
+    var video1 = document.querySelector("#video1")
 var playbtn = document.querySelector("#play")
 video1.addEventListener("mouseenter", function(){
     gsap.to(playbtn,{
@@ -6,3 +7,39 @@ video1.addEventListener("mouseenter", function(){
         opacity:1
     })
 })
+video1.addEventListener("mouseleave", function(){
+    gsap.to(playbtn,{
+        scale:0,
+        opacity:0,
+    })
+})
+video1.addEventListener("mousemove", function(dets){
+    gsap.to(playbtn,{
+        left:dets.x-100,
+        top:dets.y-130
+    })
+})
+}
+video1animation()
+
+function loadinganimation() {
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity:0,
+        duration:1,
+        delay:0.4,
+        stagger:0.3
+    })
+
+
+
+    
+gsap.from("#page1 #video1",{
+    scale:0.8,
+    opacity:0,
+    duration:0.7,
+    delay:1.4,
+})
+}
+
+loadinganimation()
